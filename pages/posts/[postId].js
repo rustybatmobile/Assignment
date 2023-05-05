cd67ef83fd6471884f3539e06fee13f3e7c04af2
Harish Neel <contact@harishcode.com>
2023-05-05 13:07:44 +0530

    bug fixes

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,5 +1,6 @@
 const Post = ({post}) => {
     const {id, title, body} = post;
+    console.log(post, "post")
     return (
         <div>
             <span>{id}</span>
@@ -11,12 +12,21 @@ const Post = ({post}) => {
 
 export default Post; 
 
+export async function getStaticPaths() {
+    return {
+        paths: [{ params: { postId: '1' } }, { params: { postId: '2' } }],
+        fallback: false, // can also be true or 'blocking'
+    }
+}
+
 export async function getStaticProps(context) {
 
     const {params} = context;
 
-    const result = await fetch(`https://jsonplaceholder.typicode.com/post/${params.postId}`);
+    const result = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`);
     const data = await result.json();
+
+    console.log(data, "Data")
 
     return {
         props: {
